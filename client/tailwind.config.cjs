988b36f93b2d88c3c312bb14e23d5d4c681286e9
unknown <sumaiyahas2908@gmail.com>
2022-08-16 14:18:05 +0530

    add tailwind configs

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx}', './public/index.html'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#F2F5F8',
+                secondary: '#0EA5E9',
+            },
+            margins: {
+                bg: '15px',
+            }
+        },
+    },
+    plugins: [],
+};
