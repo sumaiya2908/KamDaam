7ede4e704cda885ed2ccc078148c44916867468b
unknown <sumaiyahas2908@gmail.com>
2022-08-16 14:16:22 +0530

    react boilerplate files

---
+++ client/public/images/index.js
@@ -0,0 +1,12 @@
+//logo
+import Logo from './logo.svg'
+
+//Icons
+import SearchIcon from './icons/search.svg';
+import CartIcon from './icons/cart.svg';
+import AccountIcon from './icons/account.svg';
+
+//images
+import TestImage from './test-images/test-image2.png';
+
+export {Logo, SearchIcon, CartIcon, AccountIcon, TestImage};
---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeScreen from "./Screens/HomeScreen/HomeScreen";
+
+const App = () => {
+    return (
+        <div className="app">
+            <HomeScreen />
+        </div>
+    )
+}
+
+export default App
