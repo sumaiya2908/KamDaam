b1ced174b540547209c21d5fd9016a89b9275760
unknown <sumaiyahas2908@gmail.com>
2022-08-18 12:46:09 +0530

    connect to db and import model

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+const ProductModel = require('./models/Product')
+const UserModel = require('./models/User')
 
 app.listen(3000, () => {
     console.log('Server started at port 3000')
