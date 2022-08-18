c311f199c3b2dec8d0d2e702dba82dd5ceed1c63
unknown <sumaiyahas2908@gmail.com>
2022-08-18 12:45:41 +0530

    add star/revie svg

--- client/public/images/index.js
+++ client/public/images/index.js
@@ -5,8 +5,9 @@ import Logo from './logo.svg'
 import SearchIcon from './icons/search.svg';
 import CartIcon from './icons/cart.svg';
 import AccountIcon from './icons/account.svg';
+import StarIcon from './icons/star.svg';
 
 //images
 import TestImage from './test-images/test-image2.png';
 
-export {Logo, SearchIcon, CartIcon, AccountIcon, TestImage};
+export {Logo, SearchIcon, CartIcon, AccountIcon, StarIcon, TestImage};
