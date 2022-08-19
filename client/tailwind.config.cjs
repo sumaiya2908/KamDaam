719d5a057f92d069ea635a3223980c5576820197
unknown <sumaiyahas2908@gmail.com>
2022-08-19 12:22:27 +0530

    refactor: change secondary color

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
         extend: {
             colors: {
                 primary: '#F2F5F8',
-                secondary: '#0EA5E9',
+                secondary: '#0c58c6',
             },
             margins: {
                 bg: '15px',
