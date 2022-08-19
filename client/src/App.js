5ef7e54c3e9c571fed7e44443878f01f3329a1f5
unknown <sumaiyahas2908@gmail.com>
2022-08-19 12:22:51 +0530

    add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import HomeScreen from "./Screens/HomeScreen/HomeScreen";
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Login from "./Screens/LoginScreen/Login";
 
 const App = () => {
     return (
-        <div className="app">
-            <HomeScreen />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<HomeScreen/>}></Route>
+                <Route path="/login" element={<Login/>}></Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
