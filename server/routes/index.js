7db29f3ad0cfb924547843ad7980576933d4ac76
unknown <sumaiyahas2908@gmail.com>
2022-08-24 22:32:27 +0530

    refactor: add address route to app

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
+const AddressRoute = require('./Address');
 const OrderRoute = require('./Order');
 const ProductRoute = require('./Product');
 const UserRoute = require('./User');
 
+router.use('/address', AddressRoute);
 router.use('/order', OrderRoute);
 router.use('/product', ProductRoute);
 router.use('/user', UserRoute);
