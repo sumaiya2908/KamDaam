558f4b83f78d239d0dc0d2399b8a79f622be55fe
unknown <sumaiyahas2908@gmail.com>
2022-08-25 18:13:07 +0530

    chore: add cors in app

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 require('dotenv').config();
 
@@ -15,6 +16,7 @@ connection.once('open', () => {
 const route = require('./routes/index')
 
 app.use(express.json())
+app.use(cors())
 app.use(route)
 
 app.listen(3000, () => {
