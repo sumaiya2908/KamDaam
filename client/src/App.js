148d84ccb8b1383e34f4c386179c41aa4e780f6f
unknown <sumaiyahas2908@gmail.com>
2022-09-02 12:38:58 +0530

    chore: add signup route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import HomeScreen from "./Screens/HomeScreen/HomeScreen";
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Login from "./Screens/LoginScreen/Login";
+import Signup from "./Screens/LoginScreen/Signup";
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Routes>
                 <Route exact path="/" element={<HomeScreen/>}></Route>
                 <Route path="/login" element={<Login/>}></Route>
+                <Route path="/sign-up" element={<Signup/>}></Route>
             </Routes>
         </BrowserRouter>
     )
