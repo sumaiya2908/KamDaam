8d330bf602bd5dad0f48247838cb90dcbbaf6968
unknown <sumaiyahas2908@gmail.com>
2022-09-02 12:38:10 +0530

    refactor: add submit button

--- client/src/Screens/LoginScreen/Login.jsx
+++ client/src/Screens/LoginScreen/Login.jsx
@@ -3,16 +3,19 @@ import { Logo } from '../../../public/images'
 
 function Login() {
 	return (
-		<div className="outer-container">
-			<div className='login-form-container bg-white py-8 px-16 flex justofy-center align-center'>
-				<Logo className="flex justify-center align-center" />
-				<form className="flex flex-col">
+		<div className="outer-container w-full h-screen bg-primary flex justify-center items-center rounded drop-shadow-md">
+			<div className='login-form-container bg-white py-8 px-12 drop-shadow-md rounded-md'>
+				<Logo className="form-logo text-center mb-8" />
+
+				<form className="flex flex-col" action='http://localhost:3000/user/login'>
 					<label className="form-label">
 						Email
 					</label>
 					<input className='input-field' type="email" />
 					<label className="form-label">Password</label>
 					<input className='input-field' type="password" />
+					<button type='submit' className='bg-secondary text-white p-1 hover:bg-blue-600 rounded mt-2 mb-4'>Login</button>
+					<span className='text-sm sign-up-text w-full text-gray-600 text-center'>Don't have an account? <a className='text-secondary' href='/sign-up'>Create account.</a></span>
 				</form>
 			</div>
 		</div>
