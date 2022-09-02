e26fbbd828870e20a9b556d6f32edf9c88efa2a4
unknown <sumaiyahas2908@gmail.com>
2022-09-02 19:59:32 +0530

    feat: add provider to app component

--- client/index.js
+++ client/index.js
@@ -1,9 +1,15 @@
 
 import React from "react";
 import './index.css';
+import store from "./redux/store";
 import App from "./src/App";
 import {createRoot} from "react-dom/client"
+import { Provider } from "react-redux";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App/>);
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
