0a8e5ff90f738e8f7c7c12bd4ec6a34282d26d5e
unknown <sumaiyahas2908@gmail.com>
2022-09-02 20:08:11 +0530

    chore: add index for store folder

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import './index.css';
-import store from "./redux/store";
+import store from "./store";
 import App from "./src/App";
 import {createRoot} from "react-dom/client"
 import { Provider } from "react-redux";
