e2e7d19dc4a92524c96b7d2ebc040cec505e5fe6
unknown <sumaiyahas2908@gmail.com>
2022-09-04 23:58:03 +0530

    chore: add actions for login

--- client/store/user.js
+++ client/store/user.js
@@ -1,36 +1,53 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {Axios} from 'axios'
+import Axios from 'axios'
 
 const initialUser = localStorage.getItem('user')
   ? JSON.parse(localStorage.getItem('user'))
   : null
 
-const slice = createSlice({
+const userSlice = createSlice({
     name: 'user',
     initialState: {
-        user: initialUser,
+        userInfo: initialUser,
+        loading: false,
+        error: null
     },
     reducers: {
         loginSuccess: (state, action) => {
-            state.user = action.payload;
+            state.userInfo = action.payload
+            state.loading = true
+            state.error = null
             localStorage.setItem('user', JSON.stringify(action.payload))
         },
+        loginLoading: (state, action) => {
+            state.loading = true
+            state.error = null
+        },
+        loginError: (state, action) => {
+            state.error = action.payload
+            state.loading = false
+            state.userInfo = null
+
+        },
         logoutSuccess: (state, action) => {
-            state.user = null;
+            state.userInfo = null;
             localStorage.removeItem('user')
         }
     }
 })
 
-export default slice.reducer
+export default userSlice.reducer;
 
-const {loginSuccess, logoutSuccess} = slice.actions
+const {loginSuccess, logoutSuccess} = userSlice.actions
+
 export const login = ({email, password}) => async dispatch => {
+    dispatch(loginLoading(null))
     try {
-        const {data} = await Axios.post('http://localhost:3000/user/login', {email, password})
-        dispatch(loginSuccess({email}))
+        await Axios.post('http://localhost:3000/user/login', {email, password})
+        dispatch(loginSuccess(email))
     }
     catch(err) {
+        dispatch(loginError(err.message))
         return console.log(err.message)
     }
 }
