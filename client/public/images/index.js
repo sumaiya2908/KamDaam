8314afbc914927a223124f00c116aa7ab161e8ab
unknown <sumaiyahas2908@gmail.com>
2022-09-05 09:28:25 +0530

    chore: export loader

--- client/public/images/index.js
+++ client/public/images/index.js
@@ -7,8 +7,9 @@ import SearchIcon from './icons/search.svg';
 import CartIcon from './icons/cart.svg';
 import AccountIcon from './icons/account.svg';
 import StarIcon from './icons/star.svg';
+import Loader from './icons/loader.svg'
 
 //images
 import TestImage from './test-images/test-image2.png';
 
-export {Logo, LogoGray, SearchIcon, CartIcon, AccountIcon, StarIcon, TestImage};
+export {Logo, LogoGray, SearchIcon, CartIcon, AccountIcon, StarIcon, Loader, TestImage};
