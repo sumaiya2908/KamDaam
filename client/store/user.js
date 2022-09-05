c40cbe322c6c41b8d424b70379eed0730e0f0e57
unknown <sumaiyahas2908@gmail.com>
2022-09-05 20:14:36 +0530

    feat: signup actions and reducer

--- client/store/user.js
+++ client/store/user.js
@@ -10,13 +10,15 @@ const userSlice = createSlice({
     initialState: {
         userInfo: initialUser,
         loading: false,
-        error: null
+        error: null,
+        success: false
     },
     reducers: {
         loginSuccess: (state, action) => {
             state.userInfo = action.payload
-            state.loading = true
+            state.loading = false
             state.error = null
+            state.success = true
             localStorage.setItem('user', JSON.stringify(action.payload));
         },
         loginLoading: (state, action) => {
@@ -27,6 +29,7 @@ const userSlice = createSlice({
             state.error = action.payload
             state.loading = false
             state.userInfo = null
+            state.success = false
 
         },
         logoutSuccess: (state, action) => {
@@ -34,31 +37,45 @@ const userSlice = createSlice({
             localStorage.removeItem('user')
         },
         signupSuccess: (state, action) => {
-        
+            state.success = true
+            state.error = null
+            state.loading = false
         },
         signupFail: (state, action) => {
-
+            state.success = false
+            state.error = action.payload
+            state.loading = false
         }
-
     }
 })
 
 export default userSlice.reducer;
 
-const {loginSuccess, logoutSuccess, loginError, loginLoading} = userSlice.actions
+const {loginSuccess, logoutSuccess, loginError, loginLoading, signupFail, signupSuccess} = userSlice.actions
 
 export const login = ({email, password}) => async dispatch => {
     dispatch(loginLoading(null))
     try {
-        await Axios.post('http://localhost:3000/user/login', {email, password})
+        await Axios({url: 'http://localhost:3000/user/login', data: {email, password}, method: 'post'})
         dispatch(loginSuccess(email))
     }
     catch(err) {
         dispatch(loginError(err.message))
-        return console.log(err.message)
+        console.log(err.message)
     }
 }
 
 export const logout = () => async dispatch => {
     dispatch(logoutSuccess(null))
+}
+
+export const signin = ({email, password, name}) => async dispatch => {
+    dispatch(loginLoading(null))
+    try {
+        await Axios.post("http://localhost:3000/user/register", {email, password, name} )
+        dispatch(signupSuccess(null))
+    }
+    catch(err) {
+        dispatch(signupFail(err.message))
+    }
 }
