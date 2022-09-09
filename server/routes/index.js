e78f479f47f79100fb745c6419f020682ab94f8e
unknown <sumaiyahas2908@gmail.com>
2022-09-09 13:56:19 +0530

    chore: add cart route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,11 +4,13 @@ const AddressRoute = require('./Address');
 const OrderRoute = require('./Order');
 const ProductRoute = require('./Product');
 const UserRoute = require('./User');
+const CartRouter = require('./Cart')
 
 router.use('/address', AddressRoute);
 router.use('/order', OrderRoute);
 router.use('/product', ProductRoute);
 router.use('/user', UserRoute);
+router.use('/cart', CartRouter)
 
 module.exports = router;
 
