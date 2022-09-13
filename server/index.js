0fa8423d20cda4aae25f04ddd0ce72ccf8e227a5
unknown <sumaiyahas2908@gmail.com>
2022-09-13 23:13:43 +0530

    feat: add urlendcoded middleware

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const cors = require('cors')
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -15,6 +15,7 @@ connection.once('open', () => {
 
 const route = require('./routes/index')
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(cors())
 app.use(route)
