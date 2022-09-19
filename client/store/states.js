126f8b177f891c924e0b08c8a23963c57e628c8b
unknown <sumaiyahas2908@gmail.com>
2022-09-19 13:08:46 +0530

    feat(state): add global states

---
+++ client/store/states.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const stateSlice = createSlice({
+    name: 'states',
+    initialState: {
+        loading: false,
+        error: '',
+        success: true
+    },
+    reducers: {
+        loading: (state, action) => {
+            state.loading= true,
+            state.success= false,
+            state.error = ''
+        },
+        error: (state, action) => {
+            state.loading=false,
+            state.success=false,
+            state.error=action.payload
+        },
+        success: (state, action)=> {
+            state.loading=false,
+            state.success=true,
+            state.error=''
+        }
+    }
+})
+
+export default stateSlice.reducer;
+
+export const {loading, error, success} = stateSlice.actions
+
