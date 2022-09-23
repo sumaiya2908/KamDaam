7dc68baea003fb805a6943eb175414b48e8971b0
unknown <sumaiyahas2908@gmail.com>
2022-09-23 16:55:19 +0530

    feat: add index file to import files

---
+++ client/src/Screens/index.js
@@ -0,0 +1,7 @@
+export { default as CartScreen } from './CartScreen'
+export { default as ErrorScreen } from './ErrorScreen'
+export { default as HomeScreen } from './HomeScreen'
+export { default as Login } from './Login'
+export { default as ProductScreen } from './ProductScreen'
+export { default as Signup } from './Signup'
+export { default as UserProfile } from './UserProfile'
