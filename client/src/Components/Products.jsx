3d3b10bf8a87fc7d65659d0c6952f8d21441537e
unknown <sumaiyahas2908@gmail.com>
2022-09-23 17:29:10 +0530

    feat: get products from store

--- client/src/Components/Products.jsx
+++ client/src/Components/Products.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Product from './Product'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { getProducts } from '../../store/product'
+
 
 function Products() {
+	const dispatch = useDispatch()
 
-  return (
-    <div className='products'>
-    <Product/>
-    </div>
-  )
+	const { productList } = useSelector((state) => state.product)
+	const { loading, error } = useSelector((state) => state.state)
+
+	useEffect(() => {
+		dispatch(getProducts())
+	}, [])
+
+	return (
+		<div className='products mt-8 mx-10'>
+			{productList.map((product, key) => {
+				return (
+					<Product product={product} key={key} />
+				)
+				
+			})}
+		</div>
+	)
 }
 
 export default Products
