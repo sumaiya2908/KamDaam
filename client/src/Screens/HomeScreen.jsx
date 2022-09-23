37c3aabe0e4a64ef19532efb17c1ad87a49a6b86
unknown <sumaiyahas2908@gmail.com>
2022-09-23 17:21:00 +0530

    refactor: remove navbar not used

--- client/src/Screens/HomeScreen.jsx
+++ client/src/Screens/HomeScreen.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Navbar from "../Components/Navbar";
 import Products from "../Components/Products";
 
 function HomeScreen() {
 	return (
 		<div className="HomeContainer bg-primary">
-			<Navbar />
 			<Products/>
 		</div>
 	);
